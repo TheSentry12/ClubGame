ea260dc9a7749d30bd60fff2ded751c930fb4a8f
Thesentry <ehouse22@apu.edu>
2024-02-21 20:53:59 -0800

    Program.cs
    Added

---
+++ Club2/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Person
+{
+    public string name;
+
+    public int age;
+
+    public Person() { }
+    //p
+}
---
+++ Club2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the OMNINIpotent club! \n");
+        Console.WriteLine("where only the most powerful people can be found! \n");
+        Person P1 = new Person();
+        //Person[] people = new Person[7];
+
+        //for (int peopleindex =0; peopleindex < people.Length; people
+        string[] guests = new string[]
+        {"The Beyonder","Galactus","Eternity","Oblivion","Legion" ,
+      "Hela","The Darkest Knight ","The X-Men","Dr Manhattan" ,"yo mama ugly"};
+
+        string[] clubmembers = new string[10];
+
+        int guestindex = 0;
+
+        foreach (var guest in guests)
+        {
+
+            Console.WriteLine($"Do you want to let {guest} in the club? (Y/N)");
+            string input = Console.ReadLine().ToLower();
+            if (input == "y")
+            {
+
+                clubmembers[guestindex] = guest;
+                guestindex++;
+
+                Console.WriteLine($"You have invited {guest} to the club");
+            }
+
+
+
+
+        }
+
+
+        foreach (var member in clubmembers)
+        {
+            Console.WriteLine(member);
+        }
+
+
+        Console.WriteLine("\nPress any key to exit");
+        Console.ReadKey();
+
+    }
+
+}
